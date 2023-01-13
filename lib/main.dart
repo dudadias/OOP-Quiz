a7e7155c1fe2fa1c03a217950df6d5bc34a978bb
dudadiass <diasnights@gmail.com>
2023-01-13 11:22:41 -0300

    first class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
@@ -9,6 +9,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Quiz"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Question 01",
+                style: TextStyle(fontSize: 25),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("answer01"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("answer02"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("answer03"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("answer04"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
