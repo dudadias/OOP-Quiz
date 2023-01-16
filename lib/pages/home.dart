ee8c954f3fdad3710577a632f8b20e03824b9565
dudadiass <diasnights@gmail.com>
2023-01-16 19:14:36 -0300

    next question

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/models/question.dart';
+import 'package:quiz/repository/questions.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,34 +10,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<Question> perguntas = QuestionsRepository().createQuestions(5);
+  int perguntaAtual = 0;
+
+  void proximaPergunta() {
+    setState(() {
+      if (perguntaAtual < perguntas.length - 1) {
+        perguntaAtual += 1;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "Question 01",
-            style: TextStyle(fontSize: 25),
+          Text(
+            "${perguntaAtual + 1} / ${perguntas.length}",
+            style: const TextStyle(fontSize: 20),
+          ),
+          Text(
+            perguntas[perguntaAtual].utterance,
+            style: const TextStyle(fontSize: 20),
           ),
           const SizedBox(
             height: 20,
           ),
           ElevatedButton(
-            onPressed: () {},
-            child: const Text("answer01"),
+            onPressed: () {
+              proximaPergunta();
+            },
+            child: Text(perguntas[perguntaAtual].options[0]),
           ),
           ElevatedButton(
-            onPressed: () {},
-            child: const Text("answer02"),
+            onPressed: () {
+              proximaPergunta();
+            },
+            child: Text(perguntas[perguntaAtual].options[1]),
           ),
           ElevatedButton(
-            onPressed: () {},
-            child: const Text("answer03"),
+            onPressed: () {
+              proximaPergunta();
+            },
+            child: Text(perguntas[perguntaAtual].options[2]),
           ),
           ElevatedButton(
-            onPressed: () {},
-            child: const Text("answer04"),
+            onPressed: () {
+              proximaPergunta();
+            },
+            child: Text(perguntas[perguntaAtual].options[3]),
           ),
         ],
       ),
