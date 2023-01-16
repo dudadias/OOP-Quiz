590fec2e4fdfe14534fd6c4cdc605abbb2d1afff
dudadiass <diasnights@gmail.com>
2023-01-16 17:55:54 -0300

    question model

---
+++ lib/models/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  final String utterance;
+  final String answer;
+  final List<String> options;
+
+  Question({
+    required this.utterance,
+    required this.answer,
+    required this.options,
+  });
+}
